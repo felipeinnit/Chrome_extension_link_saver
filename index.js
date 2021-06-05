d684f00fef6eb1078b17415e78413054f086d925
Felipe Ribeiro <eusouocara3000@gmail.com>
2021-06-05 09:31:15 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,25 +2,51 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
-localStorage.setItem("MyLeads", "sample")
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-inputBtn.addEventListener("click", function() {
+//get current tab on chrome
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+//button to clear the list
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+  inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    
     //clean the input field
     inputEl.value = "";
     
-    //render the add item list funciton
-    renderLeads()
-})
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
 
-function renderLeads(){
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `<li>
-                        <a target='_blank' href="${myLeads[i]}"> ${myLeads[i]} </a>
-                      </li>`
-    ulEl.innerHTML = listItems    
-}
-}
+    //render the add item list funciton
+    render(myLeads)
+})
